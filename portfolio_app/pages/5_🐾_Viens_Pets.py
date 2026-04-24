fd5ff2c654256d3aa87fc0ff7bb6a6101b02fa02
candidatoVien <candidatovien327@gmail.com>
2026-04-24 23:01:42 +0800

    Update 5_🐾_Viens_Pets.py

--- portfolio_app/pages/5_🐾_Viens_Pets.py
+++ portfolio_app/pages/5_🐾_Viens_Pets.py
@@ -6,7 +6,7 @@ st.title("My Pets(Nakshie)")
 
 # DOG SECTION
 st.subheader("🐕 Dog")
-st.image("images/Hikaro2.jpg", caption="My Dog")
+st.image("portfolio_app/images/Hikaro2.jpg", caption="My Dog")
 st.write("**Name:** Hikaro")
 st.write("**Description:** He was our first dog, his name is hikaro, he was a loving dog, but sadly he passed away last December 16, 2025.  He was my first Heartbreak")
 
@@ -15,9 +15,9 @@ st.divider()
 # CAT SECTION
 st.subheader("🐈 Cats")
 cats = [
-    {"name": "Prism", "desc": "Prism is a boy cat, he is cute, it's eyes are different, the left eye is blue and the right eye is yellow. He likes licking his self and our bedsheet as well", "img": "images/Prism.jpg"},
-    {"name": "Caelum", "desc": "This is Caelum, she is a girl, she is malambing and likes to cudddle, prism is his brother", "img": "images/Caelum.jpg"},
-    {"name": "Cosmo", "desc": "This is cosmo, our one and only black cat, the bunso in the family, he likes to sleep with us, and he loves biting me", "img": "images/Cosmo.jpg"},
+    {"name": "Prism", "desc": "Prism is a boy cat, he is cute, it's eyes are different, the left eye is blue and the right eye is yellow. He likes licking his self and our bedsheet as well", "img": "portfolio_app/images/Prism.jpg"},
+    {"name": "Caelum", "desc": "This is Caelum, she is a girl, she is malambing and likes to cudddle, prism is his brother", "img": "portfolio_app/images/Caelum.jpg"},
+    {"name": "Cosmo", "desc": "This is cosmo, our one and only black cat, the bunso in the family, he likes to sleep with us, and he loves biting me", "img": "portfolio_app/images/Cosmo.jpg"},
 ]
 
 cols = st.columns(3)
